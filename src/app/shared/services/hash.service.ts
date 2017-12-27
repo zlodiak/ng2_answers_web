f6ad610da78b47156ccff8526bdb4ee3b8fa0d3a
zlodiak <prozaik81-2@yandex.ru>
2017-12-27 17:20:33 +0300

    before new validation

---
+++ src/app/shared/services/hash.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Md5 } from 'ts-md5/dist/md5';
+
+
+@Injectable()
+export class HashService {
+
+  constructor() { }
+
+  generate(str): any{
+    return Md5.hashStr(str);
+  }
+
+  compare(str1, str2): boolean{
+    const hashStr1 = Md5.hashStr(str1);
+    const hashStr2 = Md5.hashStr(str2);
+    return hashStr1 === hashStr2;
+  }
+
+}
