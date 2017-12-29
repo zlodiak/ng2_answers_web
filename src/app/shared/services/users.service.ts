7f442272ed8e01c0551ad1ee00c0c001b70f4e1c
zlodiak <prozaik81-2@yandex.ru>
2017-12-29 15:36:26 +0300

    n

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -19,9 +19,9 @@ export class UsersService {
     return this.httpClient.post('http://localhost:3000/users', user);
   }
 
-  getUserById(id: string): Observable<User> {
+  getUserById(id: string): Observable<any> {
     console.log(id);
-    return this.httpClient.get(`http://localhost:3000/users?id=${id}`).map((users: User) => {
+    return this.httpClient.get(`http://localhost:3000/users?id=${id}`).map((users: User[]) => {
       // console.log(users[0]);
       return users[0] ? users[0] : undefined;
     });
